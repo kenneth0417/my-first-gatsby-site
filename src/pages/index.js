7e70e04d70653a8b3dd8b5326bb9d1b0d48ae25a
Kenneth Gapi <kenneth.gapi@bld.ai>
2022-10-05 14:57:36 +0800

    Finished Gatsby Tutorial Part 3

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,21 @@
 import * as React from "react";
 import { Link } from "gatsby";
 import Layout from "../components/layout";
+import { StaticImage } from "gatsby-plugin-image";
 
 const IndexPage = () => {
   return (
-    <Layout pageTitle={"Home Page"}><p>I'm making this Gatsby Tutorial.</p></Layout>
+    <Layout pageTitle={"Home Page"}>
+      <p>I'm making this Gatsby Tutorial.</p>
+      {/* <StaticImage
+        alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
+        src="https://pbs.twimg.com/media/E1oMV3QVgAIr1NT?format=jpg&name=large"
+      /> */}
+      <StaticImage
+        alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
+        src="../images/cat.jpg"
+      />
+    </Layout>
   );
 };
 
