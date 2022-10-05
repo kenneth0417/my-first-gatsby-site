3fb1be8e159ff01ba2c774d7c4a6b9d10e9f5878
Kenneth Gapi <kenneth.gapi@bld.ai>
2022-10-05 20:55:01 +0800

    Finished Gatsby Tutorial Part 5

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,31 +1,37 @@
 import * as React from "react";
+import { graphql } from "gatsby";
 import Layout from "../components/layout";
 import Seo from "../components/seo";
-import { graphql } from "gatsby";
 
 const BlogPage = ({ data }) => {
-  console.log(data);
   return (
     <Layout pageTitle="My Blog Posts">
-      <ul>
-        {data.allFile.nodes.map((file, idx) => (
-          <li key={idx}>{file.name}</li>
-        ))}
-      </ul>
+      {data.allMdx.nodes.map((node) => (
+        <article key={node.id}>
+          <h2>{node.frontmatter.title}</h2>
+          <p>Posted: {node.frontmatter.date}</p>
+          <p>{node.excerpt}</p>
+        </article>
+      ))}
     </Layout>
   );
 };
 
 export const query = graphql`
   query {
-    allFile {
+    allMdx(sort: { fields: frontmatter___date, order: DESC }) {
       nodes {
-        name
+        frontmatter {
+          title
+          date(formatString: "MMMM DD, YYYY")
+        }
+        id
+        excerpt
       }
     }
   }
 `;
 
-export default BlogPage;
-
 export const Head = () => <Seo title="My Blog Posts" />;
+
+export default BlogPage;
